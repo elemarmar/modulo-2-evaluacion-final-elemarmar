bd963ad424ab112e0112ab597fb8bce56994f253
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-07-10 13:40:13 +0200

    Set up express server

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const { response } = require('express');
 
 const app = express();
 
-app.listen(3000);
+app.get('/', function (req, res) {
+    res.sendFile(__dirname+ '/index.html');
+})
+
+app.listen(3000, function () {
+    console.log('Server started on port 3000');
+});
