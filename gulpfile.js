70a41becf2525469383e2d281b59e3b5e290a663
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-07-13 10:43:55 +0200

    Change gulp-uglify for gulp-uglify-es

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
+const uglify = require('gulp-uglify-es');
 
 // Compile scss into css
 function style() {
